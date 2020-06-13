58a5d14f873854697f264b710bcfb01fb04b918f
Dharaneeshwar <daranip@gmail.com>
2020-06-13 11:23:05 +0530

    faq done

--- js/faq.js
+++ js/faq.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     const listgrp=document.getElementById("lstgrp");
     
-    var testlabs = (function() {
+    var faqs = (function() {
         var json = null;
         $.ajax({
           'async': false,
           'global': false,
-          'url': "json/test_labs.json",
+          'url': "json/faq.json",
           'dataType': "json",
           'success': function(data) {
             json = data;
@@ -15,16 +15,16 @@ $(document).ready(function(){
         return json;
       })();
 
-    tollnums =  testlabs.data;
-    tollnums.forEach(element => { 
+    faqs =  faqs.faq;
+    console.log(faqs);
+    faqs.forEach(element => { 
     listgrp.innerHTML+=
     `<div class="list-group-item list-group-item-action flex-column align-items-start " style="padding-bottom: 20px;">
     <div class="d-flex w-100 justify-content-between">
-      <h5><b>${element.name}</b></h5>
+      <h5><b>${element["s.no"]}. ${element.question}</b></h5>
     </div>
     <br>
-    <p class="mb-1"><i class="fas fa-map-marker" style="margin-right: 15px;"> </i>   ${element.location}</p>
-    <p class="mb-1"><i class="fas fa-tag" style="margin-right: 15px;"> </i>   ${element.type}</p>
+    <p class="mb-1"> ${element.answer}</p>
     </div>`;
     });   
 });

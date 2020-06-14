319a6924823b941c04dbe643a1d6b89f6417d1d6
Dharaneeshwar <daranip@gmail.com>
2020-06-14 11:31:14 +0530

    myhealthstatus base-template ready

---
+++ js/healthstatus.js
@@ -0,0 +1,44 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first --> 
+$(document).ready(function() {
+
+    var firebaseConfig= (function() {
+        var json = null;
+        $.ajax({
+          'async': false,
+          'global': false,
+          'url': "json/fb_config.json",
+          'dataType': "json",
+          'success': function(data) {
+            json = data;
+          }
+        }); 
+        return json;
+      })();
+
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  
+    //get elements 
+    console.log('init index');
+
+    const logout=document.getElementById('logout');    
+    logout.addEventListener('click', e=>{
+        //get details 
+        const auth = firebase.auth().signOut();  
+        window.location = "signinwithphno.html";
+        
+    })
+      
+    firebase.auth().onAuthStateChanged (firebaseUser => {
+        if (firebaseUser){
+            console.log(firebaseUser.email);
+        }
+        else{
+            console.log('not logged in');
+            // window.location = "signinwithphno.html";
+        }
+    });
+  // write your code here
+  
+  }());

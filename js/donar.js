a3d1ca7f8992972912f39b032dead7f4d4489c59
Dharaneeshwar <daranip@gmail.com>
2020-06-14 21:15:16 +0530

    donar module done

--- js/donar.js
+++ js/donar.js
@@ -30,13 +30,14 @@ $(document).ready(function () {
   fb_ref.on("child_added", (snap) => {
     // console.log(snap.val());
     element= snap.val();
-      if (element.type == "Free Food") {
+      if (true) {
         listgrp.innerHTML += `<div class="list-group-item flex-column align-items-start "
         style="padding-bottom: 20px;">
         <div class="d-flex w-100 justify-content-between">
             <h5><b>${element.name}</b></h5>
         </div>
         <br>
+        <p class="mb-1"><i class="fas fa-hashtag" style="margin-right: 15px;"> </i> ${element.type}</p>
         <p class="mb-1"><i class="fas fa-tag" style="margin-right: 15px;"> </i> ${element.desc}</p>
         <p class="mb-1"><i class="fas fa-map-marker-alt" style="margin-right: 15px;"> </i>${element.address}</p>
         <p class="mb-1"><i class="fas fa-phone-alt" style="margin-right: 15px;"> </i>${element.phone_number}</p>
